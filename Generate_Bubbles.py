3c31cad236f5dcc9fd86742d966beb937e36fde3
Jambako_o <71858002+yichenfromhyrule@users.noreply.github.com>
2021-04-29 22:41:51 -0400

    Create Generate_Bubbles.py

---
+++ Generate_Bubbles.py
@@ -0,0 +1,32 @@
+from tkinter import *
+def generateBubbles(canvas, bubbleList):
+    # It seems no information about canvas setting, so I create a canvas w.
+    #master = Tk()
+    #canvas_width = 500 # I set it 300, you can change
+    #canvas_height = 500
+    #w = Canvas(master, width=canvas_width,height=canvas_height)
+    #w.pack()
+
+    for bubbleDetails in bubbleList:
+        left_coordinate = bubbleDetails["left"]
+        top_coordinate = bubbleDetails["top"]
+        diameter_size = bubbleDetails["size"]
+        bubble_color = bubbleDetails["color"]
+        x0 = left_coordinate
+        y0 = top_coordinate
+        x1 = left_coordinate + diameter_size
+        y1 = top_coordinate + diameter_size
+        canvas.create_oval(x0,y0,x1,y1, fill=bubble_color)
+    mainloop()
+
+
+if __name__ == '__main__':
+    bubbleList1 = [{"left": 150, "top":150, "size": 100, "color": "green"}]
+    bubbleList2 = [
+        {"left": 317, "top":269, "size": 45, "color": "red"},
+        {"left": 118, "top":27, "size": 90, "color": "orange"},
+        {"left": 101, "top":321, "size": 65, "color": "yellow"},
+        {"left": 231, "top":219, "size": 25, "color": "pink"},
+        {"left": 50, "top":12, "size": 20, "color": "blue"}
+    ]
+    generateBubbles(canvas,bubbleList2)
